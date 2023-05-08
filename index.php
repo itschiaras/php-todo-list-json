67746ce00ef9b9ee5034eb3cd4910cfde36f0c2b
ChiaraS97 <chiara.sucato97@gmail.com>
2023-05-08 14:51:02 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <title>ToDo List</title>
+</head>
+<body>
+    <div id="app">
+        <h1>{{title}}</h1>
+    </div>
+
+
+
+    <script src="js/script.js"></script>
+</body>
+</html>
